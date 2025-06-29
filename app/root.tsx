b172beaedcec0dbf7c327f07b400d5e8cb584f9d
Binalfew Kassa <binalfew@yahoo.com>
2025-06-29 18:58:00 +0300

    change theme color

--- app/root.tsx
+++ app/root.tsx
@@ -29,6 +29,7 @@ import {
 } from 'remix-utils/csrf/react'
 import { HoneypotProvider } from 'remix-utils/honeypot/react'
 import faviconAssetUrl from './assets/favicon.svg'
+import logoAssetUrl from './assets/logo.svg'
 import { GeneralErrorBoundary } from './components/error-boundary'
 import { useToast } from './components/toaster'
 import { Icon, href as iconsHref } from './components/ui/icon'
@@ -211,7 +212,11 @@ export default function App() {
 											to="/"
 											className="flex items-center font-semibold text-primary-foreground"
 										>
-											<Icon name="emblem" className="h-16 w-16" />
+											<img
+												src={logoAssetUrl}
+												alt="Logo"
+												className="h-16 w-16"
+											/>
 											<span className="text-sm">Accreditation</span>
 										</Link>
 									</div>
@@ -385,7 +390,7 @@ export default function App() {
 										to="/"
 										className="flex items-center gap-2 font-semibold text-primary-foreground"
 									>
-										<Icon name="emblem" className="h-16 w-16" />
+										<img src={logoAssetUrl} alt="Logo" className="h-16 w-16" />
 										<span className="text-sm">Accreditation</span>
 									</Link>
 								)}
